b9e7f32997510fd8e229df73f06e63d8b82f038c
edwardowens <eddiebeme@gmail.com>
2025-11-24 16:05:29 -0800

    feat: Initial commit of SDK.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/skiff-sh/sdk-go
+
+go 1.25.4
+
+require github.com/skiff-sh/api/go v0.0.0-20251125000145-f6e1d88b9355 // indirect
---
+++ skiff/sdk.go
@@ -0,0 +1,20 @@
+package skiff
+
+import (
+	"context"
+	"io/fs"
+
+	"github.com/skiff-sh/api/go/skiff/plugin/v1alpha1"
+)
+
+type Context struct {
+	Ctx context.Context
+	// The root of the project.
+	Root fs.FS
+}
+
+// Plugin is capable of performing operations on files
+type Plugin interface {
+	// WriteFile handle a request to edit a singular file within the user's project.
+	WriteFile(ctx *Context, req *v1alpha1.WriteFileRequest) (*v1alpha1.WriteFileResponse, error)
+}
